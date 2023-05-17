db5af60fd4f50b48176ee76b94345730063e7ddb
“Lissa <lwarshaw674@gmail.com>
2023-05-17 11:04:42 -0400

    revised code based off of screens file

--- digigram/src/components/Profile.jsx
+++ digigram/src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './Profile.css';
 
-const ProfileComponent = () => {
+const Profile = ({ username, avatar }) => {
   const [userData, setUserData] = useState(null);
   const [userPosts, setUserPosts] = useState([]);
 
@@ -32,31 +33,42 @@ const ProfileComponent = () => {
     }
   };
 
-  if (!userData) {
-    return <div>Loading user data...</div>;
-  }
-
   return (
     <div>
-      <h1>{userData.username}'s Profile</h1>
-
-      <h2>User Information</h2>
-      <p>Username: {userData.username}</p>
-      <p>Email: {userData.email}</p>
-
-      <h2>Posts</h2>
-      {userPosts.length > 0 ? (
-        userPosts.map((post) => (
-          <div key={post.id}>
-            <h3>{post.caption}</h3>
-            <p>{post.post}</p>
+      <div>
+        <img src={avatar} alt={`${username} avatar`} />
+        <div>{username}</div>
+        <button>Follow</button>
+      </div>
+      <div>
+        {userData && (
+          <div>
+            <div>
+              <div>{userData.posts}</div>
+              <div>Posts</div>
+            </div>
+            <div>
+              <div>{userData.followers}</div>
+              <div>Followers</div>
+            </div>
+            <div>
+              <div>{userData.following}</div>
+              <div>Following</div>
+            </div>
           </div>
-        ))
-      ) : (
-        <p>No posts found.</p>
-      )}
+        )}
+      </div>
+      <div>
+        {userPosts.length > 0 ? (
+          userPosts.map((post, index) => (
+            <img key={index} src={post} alt={`${username} post`} />
+          ))
+        ) : (
+          <div>No Photos</div>
+        )}
+      </div>
     </div>
   );
 };
 
-export default ProfileComponent;
+export default Profile;
