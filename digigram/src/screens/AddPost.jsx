88ff88d0159dcd9e18554800248c6ecbb38d310a
Jacquelyn <jaxlynhyde@gmail.com>
2023-05-17 13:55:07 -0400

    AddPost screen and css updated

--- digigram/src/screens/AddPost.jsx
+++ digigram/src/screens/AddPost.jsx
@@ -3,44 +3,63 @@ import { createPost } from "../services/post";
 import './AddPost.css'
 
 function AddPost(props) {
-  const [post, setPost] = useState({
-    image: '',
-    caption: '',
-  })
-  // const [image, setImage] = useState("");
-  // const [caption, setCaption] = useState("");
+  const [image, setImage] = useState(null);
+  const [caption, setCaption] = useState('');
+  const [imagePreview, setImagePreview] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const post = {
+      image: image,
+      caption: caption,
+    };
     await createPost(post);
+    setImage(null);
+    setCaption('');
+    setImagePreview(null);
   };
 
-  const handleChange = (event) => {
-    setPost({...post, [event.target.name]: event.target.value,});
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    setImage(file);
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImagePreview(reader.result);
+    };
+    reader.readAsDataURL(file);
   };
 
-  const { image, caption } = post
+  const handleCaptionChange = (event) => {
+    const caption = event.target.value;
+    setCaption(caption);
+  };
 
   return (
     <form onSubmit={handleSubmit} className="add-post-form">
       <h2>Add a New Post</h2>
+      {imagePreview && (
+        <div className="add-post-form__preview">
+          <img src={imagePreview} alt="Preview" />
+        </div>
+      )}
       <div className="add-post-form__input">
-        <label htmlFor="image-input">Image URL</label>
+        <label htmlFor="image-input">Image</label>
         <input
           id="image-input"
-          type="text"
-          value={image}
-          onChange={handleChange}
-          placeholder="../digi10.png"
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
           required
         />
       </div>
+
       <div className="add-post-form__input">
         <label htmlFor="caption-input">Caption</label>
         <textarea
           id="caption-input"
           value={caption}
-          onChange={handleChange}
+          onChange={handleCaptionChange}
           placeholder="Write a caption..."
           required
         />
