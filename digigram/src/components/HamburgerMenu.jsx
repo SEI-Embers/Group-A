c0348e171968f4e723a71fa5645c25417fbd178f
“Lissa <lwarshaw674@gmail.com>
2023-05-18 12:05:16 -0400

    updated css for mobile

--- digigram/src/components/HamburgerMenu.jsx
+++ digigram/src/components/HamburgerMenu.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { slide as Menu } from 'react-burger-menu'; 
 import { Link } from 'react-router-dom';
 import './HamburgerMenu.css';
- 
+
 const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeMenu = () => setIsOpen(false);
+  const handleStateChange = (state) => setIsOpen(state.isOpen);
+
   return (
-    <Menu>
-      <Link to="/" id="home" className="menu-item">Home</Link>
-      <Link to="/addpost" id="addpost" className="menu-item">Add Post</Link>
-      <Link to="/profile" id="profile" className="menu-item">Profile</Link>
-      <Link to="/settings" id="settings" className="menu-item">Settings</Link>
-      <Link to="/sign-out" id="sign-out" className="menu-item">Sign Out</Link>
+    <Menu isOpen={isOpen} onStateChange={handleStateChange}>
+      <Link onClick={closeMenu} to="/" id="home" className="menu-item">Home</Link>
+      <Link onClick={closeMenu} to="/addpost" id="addpost" className="menu-item">Add Post</Link>
+      <Link onClick={closeMenu} to="/profile" id="profile" className="menu-item">Profile</Link>
+      <Link onClick={closeMenu} to="/settings" id="settings" className="menu-item">Settings</Link>
+      <Link onClick={closeMenu} to="/sign-out" id="sign-out" className="menu-item">Sign Out</Link>
     </Menu>
   );
 };
--- digigram/src/components/Header.jsx
+++ digigram/src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
-import logo from '../logo.png';
-import addPostIcon from '../addpost.png';
-import homeIcon from '../home.png';
-import profileIcon from '../profile.png';
-import settingsIcon from '../settings.png';
+import logo from "../logo.png";
+import addPostIcon from "../addpost.png";
+import homeIcon from "../home.png";
+import profileIcon from "../profile.png";
+import settingsIcon from "../settings.png";
 import HamburgerMenu from "./HamburgerMenu";
 
 const Header = () => {
   return (
     <div className="header">
       <Link to="/" className="logo">
+        <HamburgerMenu />
         <img className="logo-icon" src={logo} alt="Logo" />
       </Link>
       <div className="menu">
@@ -30,7 +31,6 @@ const Header = () => {
         <Link to="/sign-out" className="menu-item">
           Sign Out
         </Link>
-        <HamburgerMenu />
       </div>
     </div>
   );
